a024a57dcfd20ae457c05d978cb9b07b7934bd2a
Raymond Gigliotti <ray@firestitch.com>
2019-05-01 10:17:17 -0400

    exposed ExampleService

--- src/public_api.ts
+++ src/public_api.ts
@@ -7,5 +7,6 @@ export { FsExampleModule } from './app/fs-example.module';
 
 export { ExampleRef } from './app/example-ref';
 
-
 export { FsExampleComponent } from './app/components/fs-example/fs-example.component';
+
+export { ExampleService } from './app/services/example.service';
