5e27f76074514ae728e3daf3488a3ef4424996eb
Raymond Gigliotti <ray@firestitch.com>
2024-10-03 13:25:23 -0400

    import { FsExampleModule } from '../../src/app/fs-example.module';

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FsExampleModule } from '@firestitch/example';
+import { FsExampleModule } from '../../src/app/fs-example.module';
 
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './material.module';
